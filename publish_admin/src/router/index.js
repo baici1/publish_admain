8df8bdd555af068ffa2179718be186db8bdad07d
baici1 <249337001@qq.com>
2021-01-08 17:26:10 +0800

    2021/1/8

--- publish_admin/src/router/index.js
+++ publish_admin/src/router/index.js
@@ -1,10 +1,20 @@
+// 你写的页面添加路由
+import Login from '@/views/login'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 // 路由配置地方
 const routes = [
-
+  {
+    path: '/',
+    redirect: '/login'// 重定向地址
+  },
+  {
+    path: '/login', // 显示地址
+    name: 'login',
+    component: Login// 组件名称
+  }
 ]
 
 const router = new VueRouter({
---
+++ publish_admin/src/views/login/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
