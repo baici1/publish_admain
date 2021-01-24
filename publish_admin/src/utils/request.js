f2ff2c4661773bb13eb5fc3f2a2a0fbba7a79cb0
baici1 <249337001@qq.com>
2021-01-24 11:11:47 +0800

    D

--- publish_admin/src/utils/request.js
+++
@@ -1,9 +0,0 @@
-// 基于axios请求
-import axios from 'axios'
-
-// 我们通过这个实例去发请求，把需要的配置配置给这个实例来处理
-const request = axios.create({
-  baseURL: 'http://api-toutiao-web.itheima.net/' // 请求的基础路径
-})
-// 导出请求方法
-export default request
